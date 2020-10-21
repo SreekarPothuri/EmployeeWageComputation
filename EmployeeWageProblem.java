4cd2f160369e0923c3d1d93ea714733a5a2f433f
Your Name <you@example.com>
2020-10-21 17:32:41 +0530

    Added:Employee is Present/Absent

--- EmployeeWageProblem.java
+++ EmployeeWageProblem.java
@@ -1,5 +1,10 @@
 public class EmployeeWageProblem{
 	public static void main(String args[]){
 		System.out.println("*****WELCOME TO EMPLOYEE WAGE PROBLEM*****");
+		double EmpCheck = Math.floor(Math.random() * 10) % 2;
+		if(EmpCheck == 1)
+			System.out.println("Employee is present");
+		else
+			System.out.println("Employee is absent");
 	}
 }
