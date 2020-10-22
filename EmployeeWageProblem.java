f4e34a1eef8ee33e1c42a7cf3ca56dbfb8661160
Your Name <you@example.com>
2020-10-22 16:18:14 +0530

    Added: Total Wage provided by a company

--- EmployeeWageProblem.java
+++ EmployeeWageProblem.java
@@ -2,12 +2,28 @@ public class EmployeeWageProblem {
 	public static final int IS_FULL_TIME=1;
         public static final int IS_PART_TIME=2;
 
-        public static int computeEmpWage(String company, int WagePerHr, int Max_WorkingDaysPerMonth, int Max_WorkingHrsPerMonth) {
-                int HrsPerDay=0;
+	private final String COMPANY;
+	private final int WAGE_PER_HR;
+	private final int MAX_WORKINGDAYS_PER_MONTH;
+	private final int MAX_WORKINGHRS_PER_MONTH;
+	private final int NUM_OF_EMPLOYEES;
+	private int MonthlyWage;
+	private int TotalWage;
+
+	public EmployeeWageProblem(String COMPANY, int WAGE_PER_HR, int MAX_WORKINGDAYS_PER_MONTH, int MAX_WORKINGHRS_PER_MONTH,int NUM_OF_EMPLOYEES) {
+		this.COMPANY = COMPANY;
+		this.WAGE_PER_HR = WAGE_PER_HR;
+		this.MAX_WORKINGDAYS_PER_MONTH = MAX_WORKINGDAYS_PER_MONTH;
+		this.MAX_WORKINGHRS_PER_MONTH = MAX_WORKINGHRS_PER_MONTH;
+		this.NUM_OF_EMPLOYEES = NUM_OF_EMPLOYEES;
+	}
+
+        public void computeEmpWage() {
+		int HrsPerDay=0;
                 int WorkingHrs=0;
                 int Day=0;
 
-                while(WorkingHrs <= Max_WorkingHrsPerMonth && Day < Max_WorkingDaysPerMonth) {
+                while(WorkingHrs <= MAX_WORKINGHRS_PER_MONTH && Day < MAX_WORKINGDAYS_PER_MONTH) {
                         Day++;
                         int empCheck = (int) Math.floor(Math.random() * 10) % 3;
                         switch(empCheck) {
@@ -27,17 +43,32 @@ public class EmployeeWageProblem {
                         WorkingHrs += HrsPerDay;
                         System.out.println("\t"+"Emp Hr : " +HrsPerDay);
                 }
-                int MonthlyWage = WorkingHrs * WagePerHr;
-                System.out.println("******Total Emp Wage for Company: " +company+ " is "+MonthlyWage+"**************");
-                return MonthlyWage;
+                MonthlyWage = WorkingHrs * WAGE_PER_HR;
+		System.out.println("Monthly Wage of an employee is "+MonthlyWage);
+		TotalWage = MonthlyWage * NUM_OF_EMPLOYEES;
         }
 
+	@Override
+	public String toString() {
+		return "*****Total Wage for Company " +COMPANY+ " is "+TotalWage+"*****\n";
+	}
+
         public static void main(String args[]) {
-		System.out.println("*****WELCOME TO EMPLOYEE WAGE PROBLEM*****");
-                computeEmpWage("PrimeVideo",10,2,10);
-                computeEmpWage("Netflix",20,4,15);
-                computeEmpWage("BridgeLabz",30,6,20);
-                computeEmpWage("Amazon",40,8,25);
-                computeEmpWage("Microsoft",50,10,30);
+		EmployeeWageProblem TCS = new EmployeeWageProblem("TCS",10,10,40,10);
+		EmployeeWageProblem Infosys = new EmployeeWageProblem("Infosys",20,12,48,20);
+		EmployeeWageProblem BridgeLabz = new EmployeeWageProblem("BridgeLabz",30,14,52,30);
+		EmployeeWageProblem Amazon = new EmployeeWageProblem("Amazon",40,16,60,40);
+		EmployeeWageProblem Netflix = new EmployeeWageProblem("Netflix",50,20,60,50);
+
+                TCS.computeEmpWage();
+		System.out.println(TCS);
+                Infosys.computeEmpWage();
+		System.out.println(Infosys);
+                BridgeLabz.computeEmpWage();
+		System.out.println(BridgeLabz);
+                Amazon.computeEmpWage();
+		System.out.println(Amazon);
+                Netflix.computeEmpWage();
+		System.out.println(Netflix);
         }
 }
