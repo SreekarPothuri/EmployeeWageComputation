efbb85fc679c47a09b08f76858c601b437458a91
Your Name <you@example.com>
2020-10-23 08:40:59 +0530

    Refactored: CompanyEmployeeWage Class Introduced

--- EmployeeWageProblem.java
+++ EmployeeWageProblem.java
@@ -2,28 +2,33 @@ public class EmployeeWageProblem {
 	public static final int IS_FULL_TIME=1;
         public static final int IS_PART_TIME=2;
 
-	private final String COMPANY;
-	private final int WAGE_PER_HR;
-	private final int MAX_WORKINGDAYS_PER_MONTH;
-	private final int MAX_WORKINGHRS_PER_MONTH;
-	private final int NUM_OF_EMPLOYEES;
-	private int MonthlyWage;
 	private int TotalWage;
 
-	public EmployeeWageProblem(String COMPANY, int WAGE_PER_HR, int MAX_WORKINGDAYS_PER_MONTH, int MAX_WORKINGHRS_PER_MONTH,int NUM_OF_EMPLOYEES) {
-		this.COMPANY = COMPANY;
-		this.WAGE_PER_HR = WAGE_PER_HR;
-		this.MAX_WORKINGDAYS_PER_MONTH = MAX_WORKINGDAYS_PER_MONTH;
-		this.MAX_WORKINGHRS_PER_MONTH = MAX_WORKINGHRS_PER_MONTH;
-		this.NUM_OF_EMPLOYEES = NUM_OF_EMPLOYEES;
+	private int numOfCompanies = 0;
+	private CompanyEmployeeWage[] CompanyEmployeeWageArray;
+
+	public EmployeeWageProblem(){
+		CompanyEmployeeWageArray = new CompanyEmployeeWage[5];
 	}
 
-        public void computeEmpWage() {
+	private void addCompanyEmployeeWage(String COMPANY,int WAGE_PER_HR,int MAX_WORKINGDAYS_PER_MONTH,int MAX_WORKINGHRS_PER_MONTH,int NUM_OF_EMPLOYEES){
+		CompanyEmployeeWageArray[numOfCompanies] = new CompanyEmployeeWage(COMPANY,WAGE_PER_HR,MAX_WORKINGDAYS_PER_MONTH,MAX_WORKINGHRS_PER_MONTH,NUM_OF_EMPLOYEES);
+		numOfCompanies++;
+	}
+
+	private void computeEmpWage(){
+		for(int i=0;i<numOfCompanies;i++){
+			CompanyEmployeeWageArray[i].setMonthlyWage(this.computeEmpWage(CompanyEmployeeWageArray[i]));
+			System.out.println(CompanyEmployeeWageArray[i]);
+		}
+	}
+
+        private int computeEmpWage(CompanyEmployeeWage CompanyEmployeeWage) {
 		int HrsPerDay=0;
                 int WorkingHrs=0;
                 int Day=0;
 
-                while(WorkingHrs <= MAX_WORKINGHRS_PER_MONTH && Day < MAX_WORKINGDAYS_PER_MONTH) {
+                while(WorkingHrs <= CompanyEmployeeWage.MAX_WORKINGHRS_PER_MONTH && Day < CompanyEmployeeWage.MAX_WORKINGDAYS_PER_MONTH) {
                         Day++;
                         int empCheck = (int) Math.floor(Math.random() * 10) % 3;
                         switch(empCheck) {
@@ -43,32 +48,18 @@ public class EmployeeWageProblem {
                         WorkingHrs += HrsPerDay;
                         System.out.println("\t"+"Emp Hr : " +HrsPerDay);
                 }
-                MonthlyWage = WorkingHrs * WAGE_PER_HR;
-		System.out.println("Monthly Wage of an employee is "+MonthlyWage);
-		TotalWage = MonthlyWage * NUM_OF_EMPLOYEES;
+                CompanyEmployeeWage.MonthlyWage = WorkingHrs * CompanyEmployeeWage.WAGE_PER_HR;
+		TotalWage = CompanyEmployeeWage.MonthlyWage * CompanyEmployeeWage.NUM_OF_EMPLOYEES;
+		System.out.println("Total Wage for Company is "+TotalWage);
+		return CompanyEmployeeWage.MonthlyWage;
         }
 
-	@Override
-	public String toString() {
-		return "*****Total Wage for Company " +COMPANY+ " is "+TotalWage+"*****\n";
-	}
-
         public static void main(String args[]) {
-		EmployeeWageProblem TCS = new EmployeeWageProblem("TCS",10,10,40,10);
-		EmployeeWageProblem Infosys = new EmployeeWageProblem("Infosys",20,12,48,20);
-		EmployeeWageProblem BridgeLabz = new EmployeeWageProblem("BridgeLabz",30,14,52,30);
-		EmployeeWageProblem Amazon = new EmployeeWageProblem("Amazon",40,16,60,40);
-		EmployeeWageProblem Netflix = new EmployeeWageProblem("Netflix",50,20,60,50);
+		EmployeeWageProblem e = new EmployeeWageProblem();
 
-                TCS.computeEmpWage();
-		System.out.println(TCS);
-                Infosys.computeEmpWage();
-		System.out.println(Infosys);
-                BridgeLabz.computeEmpWage();
-		System.out.println(BridgeLabz);
-                Amazon.computeEmpWage();
-		System.out.println(Amazon);
-                Netflix.computeEmpWage();
-		System.out.println(Netflix);
+		e.addCompanyEmployeeWage("TCS",10,10,40,10);
+		e.addCompanyEmployeeWage("Infosys",20,12,48,20);
+		e.addCompanyEmployeeWage("BridgeLabz",30,14,52,30);
+		e.computeEmpWage();
         }
 }
